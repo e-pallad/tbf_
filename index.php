e2ede02e7d9f77891773c5d1680797872b7f53fd
Eric <60659440+e-pallad@users.noreply.github.com>
2020-04-10 19:11:38 +0200

    max input symbols for titel 25>30

--- index.php
+++ index.php
@@ -157,7 +157,7 @@
             };
            ?>
         </select></p><br>
-        <p>Titel: <input type="text" name="add_text" pattern="[0-9a-zA-ZäöüÄÖÜ _-]{0,25}" placeholder="max. 25 Zeichen" /></p><br>
+        <p>Titel: <input type="text" name="add_text" pattern="[0-9a-zA-ZäöüÄÖÜ _-]{0,30}" placeholder="max. 30 Zeichen" /></p><br>
         <input type="submit" name="submit" value="Dateiname generieren" />
       </form>
     </div>
