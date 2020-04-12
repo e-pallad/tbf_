73b5511ec6728e2878616d46aad892d80c9f57b6
Eric <60659440+e-pallad@users.noreply.github.com>
2020-04-12 14:45:07 +0200

    added missed :

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     input::-webkit-outer-spin-button, input::-webkit-inner-spin-button {-webkit-appearance: none; margin: 0;}
     input[type=number] {-moz-appearance:textfield;}
     .csv-button {margin-right: 5px;}
-    .copyright {font-size: 12px; position: fixed; bottom 0;}
+    .copyright {font-size: 12px; position: fixed; bottom: 0;}
     .copyright a {color: black; text-decoration: none;}
   </style>
   <body>
